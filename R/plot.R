7f7df16527f937b9df8674055d4312500298e1ee
reyvnth <therevanthkrishna@gmail.com>
2025-05-06 01:56:32 -0600

    v0.7 update
    - plotcontrastTree() bug fix

--- R/plot.R
+++ R/plot.R
@@ -981,7 +981,7 @@ plotContrastTree <- function(d.counts, d.groups, ref.level, target.level, plot.t
   
   # Positions of inner nodes
   innode.pos <- unique(node.pos[,c('x','y','id')])
-  rownames(innode.pos) <- innode.pos$id
+  rownames(innode.pos) <- make.unique(as.character(innode.pos$id))
   # Ranges of inner nodes (length of branches)
   innode.pos$range <- -1
   for (i in 1:nrow(innode.pos)){
